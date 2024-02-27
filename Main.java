776e77c4e962d908b2ec8fb5b8841cf1a3e237a3
Marmota_in_coma <40302782+Mahou-sama666@users.noreply.github.com>
2024-02-27 11:38:12 +0200

    Seminar1.2

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 package g1065.seminar.seminar1;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -26,9 +27,20 @@ public class Main {
         try{
             x=stergereLinie(x, 1);
             afisareMatrice(x,"\n\nMatricea dupa stergere: ");
+            schimbareLinie(x,1,new double[]{9.99,99.9});
+            afisareMatrice(x,"Matricea dupa schimbare linia 1: ");
+            adaugareValori(x,1,new double[]{100,-100,1000,-1000});
+            afisareMatrice(x,"Matricea dupa adaugare elm pe linia 1: ");
         }
         catch(Exception e){
             System.err.println(e);
+        }
+
+        double[][][] y=citire();
+        if(y!=null){
+            for (double[][] v : y) {
+                afisareMatrice(v,"\n ");
+            }
         }
     }
 
@@ -49,6 +61,25 @@ public class Main {
         return y;
     }
 
+    private static void adaugareValori(double[][] x, int k, double[] v )throws Exception{
+        int n=x.length;
+        if(k<0 || k>=n){
+            throw new Exception("Linie invalida!");
+        }
+        double[] y=new double[x[k].length+v.length];
+        System.arraycopy(x[k],0,y,0,x[k].length);
+        System.arraycopy(v,0,y,x[k].length,v.length);
+        x[k]=y;
+    }
+    private static void schimbareLinie(double[][] x, int k, double[] v) throws Exception {
+        int n=x.length;
+        if(k<0 || k>=n){
+            throw new Exception("Linie invalida!");
+        }
+        x[k]=v;
+
+
+    }
     private static double[][] stergereLinie(double[][]x, int k) throws Exception {
         int n=x.length;
         if(k<0 || k>n) {
@@ -67,10 +98,34 @@ public class Main {
 
     private static void afisareMatrice(double[][] x, String mesaj){
         System.out.println(mesaj);
-            for (double[] v:x) {
-                System.out.println(Arrays.toString(v));
-            }
+        for (double[] v:x) {
+            System.out.println(Arrays.toString(v));
         }
+    }
+
+    private static double[][][] citire() {
+        double[][][] x=null;
+        try(Scanner scanner=new Scanner(System.in)){
+            int n=Integer.parseInt(scanner.nextLine().trim());
+            x = new double[n][][];
+            double v=0;
+            for (int i=0;i<n;i++, v+=1){
+                String[] linie=scanner.nextLine().trim().split(",");
+                x[i]=new double[linie.length][];
+                for (int j = 0; j < x[i].length; j++) {
+                    x[i][j]=new double[Integer.parseInt(linie[j].trim())];
+                    for (int k = 0; k < x[i][j].length; k++) {
+                        x[i][j][k]=v;
+                    }
+                }
+            }
+
+        }
+        catch (Exception ex){
+            System.err.println(ex);
+        }
+        return x;
+    }
 
 }
 
